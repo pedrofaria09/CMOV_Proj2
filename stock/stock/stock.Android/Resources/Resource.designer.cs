b340a69a4c51b6c314b927636c86384f3bc3349f
francisco <up201305016@fe.up.pt>
2018-12-15 17:32:20 +0000

    belo grafico

--- stock/stock/stock.Android/Resources/Resource.designer.cs
+++ stock/stock/stock.Android/Resources/Resource.designer.cs
@@ -26,6 +26,8 @@ namespace stock.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Sylapse.MainThreadDispatch.Resource.String.ApplicationName = global::stock.Droid.Resource.String.ApplicationName;
+			global::Sylapse.MainThreadDispatch.Resource.String.Hello = global::stock.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::stock.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3964,6 +3966,12 @@ namespace stock.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f0a003d
+			public const int ApplicationName = 2131361853;
+			
+			// aapt resource value: 0x7f0a003c
+			public const int Hello = 2131361852;
 			
 			// aapt resource value: 0x7f0a0015
 			public const int abc_action_bar_home_description = 2131361813;
